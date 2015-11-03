c160f8fbe288ae2e4fa93d61eda9cb180ca314ac
Alex Tideman <alex.tideman@gmail.com>
2015-11-03 13:49:37 -0700

    Add bullet, try to fix related_products query

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,9 +2,15 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin/"
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
